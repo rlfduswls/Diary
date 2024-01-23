f82bc90a6c0153d80bfa0ef58bd97dc9b284f8bf
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-01-24 00:37:48 +0900

    24 00 37 일부 수정

--- app/src/main/java/com/example/gurudiary/DBManager.kt
+++ app/src/main/java/com/example/gurudiary/DBManager.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+//회원 관리용 DB를 만드는 클래스입니다.
 class DBManager(context:Context,
                 name:String,
                 factory:SQLiteDatabase.CursorFactory?,
--- app/src/main/java/com/example/gurudiary/JoinMemberActivity.kt
+++ app/src/main/java/com/example/gurudiary/JoinMemberActivity.kt
@@ -9,12 +9,14 @@ class JoinMemberActivity : AppCompatActivity() {
 
     //회원 가입 페이지 제작 위한 액티비티
 
+    lateinit var dbMember: DBManager
     lateinit var name: EditText
     lateinit var id: EditText
     lateinit var idCheck : Button
     lateinit var pass: EditText
     lateinit var passCheck : EditText
-    lateinit var tel: EditText // 뷰에서 받아올 정보들
+    lateinit var tel: EditText
+    lateinit var submit: Button // 뷰에서 받아올 정보들
     //이 정보들을 db로 업데이트 할 예정입니다
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +28,28 @@ class JoinMemberActivity : AppCompatActivity() {
         pass = findViewById(R.id.pass)
         passCheck = findViewById(R.id.passCheck)
         tel = findViewById(R.id.tel)
+        submit = findViewById(R.id.submit)
+
+        dbMember = DBManager(this, "member", null,1)//회원관리용 db 생성
+
+        submit.setOnClickListener {
+            var str_name : String = name.text.toString()
+            var str_id : String = id.text.toString()
+            var str_pass : String = pass.text.toString()
+            var str_tel : String = tel.text.toString()
+
+            //EditText에서 받아온 정보를 memberDB에 넣어주는 작업
+
+            //제출버튼을 눌렀을 때 DB에 추가하는 기능...
+        }
+
+
 
     }
 
-    //아이디 중복 확인 버튼을 눌렀을 때 중복인지 아닌지 확인하는 기능
-
-    //제출버튼을 눌렀을 때 DB에 추가하는 기능...
-
     //일단디비를불러올게요
-    //디비이름머로할까요
+    //회원 관리 DB 이름은 member입니다
+
+    //아이디 중복 검사 함수 !! 아이디가 DB에 존재하면, 토스트 메시지를 띄웁니다.
 
 }
