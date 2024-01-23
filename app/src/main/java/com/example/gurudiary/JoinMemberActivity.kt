77872805b9afd6efc82c15c594108785ceb96dfc
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-01-23 14:36:34 +0900

    #androidx 관련 에러 문제로 몇개 import했습니다.

---
+++ app/src/main/java/com/example/gurudiary/JoinMemberActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gurudiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinMemberActivity : AppCompatActivity() {
+
+    //회원 가입 페이지 제작 위한 액티비티
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_member)
+    }
+}
--- app/src/main/java/com/example/gurudiary/MainActivity.kt
+++ app/src/main/java/com/example/gurudiary/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.gurudiary
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //메인 액티비티를 첫 화면으로 해서 로그인 액티비티로 만들까 싶어요
+        //스크롤 액티비티로 다이어리 화면을 만드시면 될 것 같습니다.
     }
 }
